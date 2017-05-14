0756136ce51d8c379cd061d9a690e3ffc1f31b53
DAVID <davidminguez97@opendeusto.es>
2017-05-14 17:39:52 +0200

    prueba

---
+++ ProCPP/prueba.cpp
@@ -0,0 +1,21 @@
+/*
+ * prueba.cpp
+ *
+ *  Created on: 14 may. 2017
+ *      Author: DAVID
+ */
+
+#include "prueba.h"
+
+namespace Punto {
+
+prueba::prueba() {
+	// TODO Auto-generated constructor stub
+
+}
+
+prueba::~prueba() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Punto */
---
+++ ProCPP/prueba.h
@@ -0,0 +1,21 @@
+/*
+ * prueba.h
+ *
+ *  Created on: 14 may. 2017
+ *      Author: DAVID
+ */
+
+#ifndef PRUEBA_H_
+#define PRUEBA_H_
+
+namespace Punto {
+
+class prueba {
+public:
+	prueba();
+	virtual ~prueba();
+};
+
+} /* namespace Punto */
+
+#endif /* PRUEBA_H_ */
