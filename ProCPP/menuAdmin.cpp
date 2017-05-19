f71a519d62e2a48c3c9fec786917562ead6a5804
surelyo <surel_97@hotmail.com>
2017-05-19 19:35:01 +0200

    Metodo consultarPedido

--- ProCPP/menuAdmin.cpp
+++ ProCPP/menuAdmin.cpp
@@ -44,6 +44,36 @@ void menuAdmin:: mostrarMenu(){
 }
 
 }
+
+void menuAdmin::eliminarEmpresa(DB *bd){//Check menuAdmin.h for info
+
+}
+
+void menuAdmin::mostrarPedidos(DB *bd){//Check menuAdmin.h for info
+	int id, opcion;
+	char *nu;
+	cout << "Va a introducir usted: \n1. Nombre de usuario\n2. ID de pedido " << endl;
+	cin >> opcion;
+	while(opcion>2||opcion<1){
+		cout << "Introduzca una opcion correcta: " << endl;
+		cin >> opcion;
+	}
+	if(opcion==1){
+		cout << "Introduzca el nombre de usuario: " << endl;
+		cin >> nu;
+		bd->recuperarPedidos(nu);
+	}
+	else if(opcion==2){
+		cout << "Introduzca el ID del pedido: " << endl;
+		cin >> id;
+		bd->recuperarPedido(id);
+	}
+}
+
+void menuAdmin::eliminarPedido(DB *bd){//Check menuAdmin.h for info
+
+}
+
 menuAdmin::~menuAdmin() {
 	// TODO Auto-generated destructor stub
 }
--- ProCPP/menuAdmin.h
+++ ProCPP/menuAdmin.h
@@ -7,6 +7,8 @@
 
 #ifndef MENUADMIN_H_
 #define MENUADMIN_H_
+#include "DB.h"
+using namespace std;
 
 
 
@@ -14,6 +16,11 @@ class menuAdmin {
 public:
 	void mostrarMenu();
 	menuAdmin();
+	//Se le pasa la BD a estos tres metodos para no tener que abrirla en la implementacion y que no cree conflicto con
+	//el main, es decir, que no se abra la BD dos veces(la del main y la del metodo)
+	void eliminarEmpresa(DB *bd);
+	void mostrarPedidos(DB *bd);
+	void eliminarPedido(DB *bd);
 	virtual ~menuAdmin();
 };
 
